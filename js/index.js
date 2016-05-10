7460fd820fc74d75b74268ac798701e7f26b8cfe
Auro Mota <palubo01@gmail.com>
2016-05-10 19:50:22 -0300

    Simple application

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var users = [];
+
+function submit() {
+    var list = document.getElementById('list');
+    var input = document.getElementById('name');
+    users.push(input.value);
+    input.value = '';
+    users = users.sort();
+    clear();
+    var ul = document.createElement('ul');
+    list.appendChild(ul);
+    for(var i=0; i<users.length; i++) {
+        var item = document.createElement('li');
+        item.appendChild(document.createTextNode(users[i]));
+        ul.appendChild(item);
+    }
+}
+
+function clear() {
+    var list = document.getElementById('list');
+    while(list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+function clearAll() {
+    clear();
+    users = [];
+}
+
+function init() {
+    document.getElementById('submit').onclick = submit;
+    document.getElementById('clear').onclick = clearAll;
+}
+
+window.onload = init;
