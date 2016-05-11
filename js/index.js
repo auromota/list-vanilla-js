cdad7658dc08f70d636fb98ff68d21705d49dc9a
Auro Mota <palubo01@gmail.com>
2016-05-10 21:27:24 -0300

    Deleting item from list

--- js/index.js
+++ js/index.js
@@ -1,38 +1,70 @@
 var users = [];
 
 function submit() {
-    var list = document.getElementById('list');
     var input = document.getElementById('name');
+    if(isValid(input)) {
+        addUser(input);
+        sortUsersByName();
+        loadPage();
+    } else {
+        alert('You must inform an username');
+    }
+}
+
+function addUser(input) {
     users.push(input.value);
     input.value = '';
+}
+
+function isValid(input) {
+    if(input.value) return true;
+    return false;
+}
+
+function sortUsersByName() {
     users = users.sort(function(a, b) {
         return a.toLowerCase().localeCompare(b.toLowerCase());
     });
-    clear();
+}
+
+function loadPage() {
+    clearPage();
+    var list = document.getElementById('list');
     var ul = document.createElement('ul');
     list.appendChild(ul);
     for(var i=0; i<users.length; i++) {
         var item = document.createElement('li');
         item.appendChild(document.createTextNode(users[i]));
+        item.setAttribute('id', 'item' + i);
+        item.onclick = deleteUser;
         ul.appendChild(item);
     }
 }
 
-function clear() {
+function deleteUser(event) {
+    var item = event.srcElement;
+    if(confirm('Do you want to delete ' + item.textContent + '?')) {
+        var id = item.id.slice(4);
+        users.splice(id, 1);
+        loadPage();
+    }
+}
+
+function clearPage() {
     var list = document.getElementById('list');
     while(list.firstChild) {
         list.removeChild(list.firstChild);
     }
 }
 
-function clearAll() {
-    clear();
+function reset() {
+    clearPage();
     users = [];
 }
 
 function init() {
     document.getElementById('submit').onclick = submit;
-    document.getElementById('clear').onclick = clearAll;
+    document.getElementById('clear').onclick = reset;
 }
 
 window.onload = init;
